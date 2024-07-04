e94a2782d05cefc94b9ba5a21406c056b48ae1eb
Tejas-Soundattikar <116431346+Tejas-Soundattikar@users.noreply.github.com>
2024-07-04 13:00:39 +0530

    Updated
    Added top 10 tracks from every platform

--- Most Streamed Spotify Songs.py
+++ Most Streamed Spotify Songs.py
@@ -23,24 +23,44 @@ print(songs.describe())
 
 print(songs['Pandora Track Stations'])
 
-
 ### Univariate Analysis - outliers detection in numerical features
 
 num_cols = ['Track Score', 'Spotify Popularity', 'Apple Music Playlist Count', 'Deezer Playlist Count', 'Amazon Playlist Count']
-
 for i in num_cols:
     sns.boxplot(data = songs[i])
     plt.title(f"Outliers for {i}")
     plt.show()
 
 
-## Top 10 most played tracks on Spotify
+## Dividing the Columns by 1000.
 
-top_10_spotify = songs[['Track', 'Spotify Streams']].sort_values('Spotify Streams',ascending=False)
+cols_to_convert = ['Spotify Streams', 'Spotify Playlist Count', 'Spotify Playlist Reach', 'YouTube Views', 'YouTube Likes', 'TikTok Posts', 'TikTok Likes', 'TikTok Views',
+                   'YouTube Playlist Reach','Apple Music Playlist Count', 'AirPlay Spins', 'SiriusXM Spins','Deezer Playlist Reach','Deezer Playlist Count','Amazon Playlist Count', 
+                   'Pandora Streams', 'Pandora Track Stations', 'Soundcloud Streams', 'Shazam Counts']
 
-top_10_spotify = top_10_spotify.head(10)
-print(top_10_spotify)
+
+for col in cols_to_convert:
+    songs[col] = songs[col].apply(lambda x: str(x).replace(",",""))
+    songs[col] = songs[col].astype(float) / 1000
+    
+## Checking for the duplicate tracks and dopping them
+
+print(songs['Track'].duplicated().sum())
+songs.drop_duplicates(subset=['Track'], inplace=True)
+
+print(songs['Track'].duplicated().sum())
+
+## Top 10 tracks on Different Platforms 
+
+for col in cols_to_convert:
+    songs.sort_values(col, ascending=False, inplace=True)
+    top_10 = songs[['Track',col]].reset_index()
+    print(f"Top 10 Tracks - {col}",top_10['Track'].head(10))
+
+    plt.figure(figsize=(12,8))
+    sns.barplot(top_10.head(10), x = 'Track', y = col, palette= 'rainbow')
+    plt.xticks(rotation = 60)
+    plt.title(f"Top 10 Tracks - {col}")
+    plt.show()
 
 
-sns.barplot(top_10_spotify, x = 'Track', y = 'Spotify Streams')
-plt.show()
