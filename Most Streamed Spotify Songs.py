a67c091fbc9bd42fb6876494f193d832e66a3ab2
Tejas-Soundattikar <116431346+Tejas-Soundattikar@users.noreply.github.com>
2024-07-05 22:13:09 +0530

    Updated
    Updated

--- Most Streamed Spotify Songs.py
+++ Most Streamed Spotify Songs.py
@@ -10,20 +10,21 @@ except UnicodeDecodeError:
     songs = pd.read_csv(path, encoding='iso-8859-1')
 
 print(songs.head())
-
 print(songs.shape)
-
 print(songs.dtypes)
-
 print(songs.nunique())
-
 print(songs.isnull().sum())
-
 print(songs.describe())
-
 print(songs['Pandora Track Stations'])
-
 print(songs['Spotify Popularity'])
+
+
+
+### Converting the Release Date columns into Datetime object.
+songs['Release Date'] = pd.to_datetime(songs['Release Date'])
+songs['Release Month'] = songs['Release Date'].dt.month
+songs['Release Year'] = songs['Release Date'].dt.year
+
 ### Univariate Analysis - outliers detection in numerical features
 
 num_cols = ['Track Score', 'Spotify Popularity', 'Apple Music Playlist Count', 'Deezer Playlist Count', 'Amazon Playlist Count']
@@ -44,12 +45,40 @@ for col in cols_to_convert:
     songs[col] = songs[col].apply(lambda x: str(x).replace(",",""))
     songs[col] = songs[col].astype(float) / 1000
     
+    
 ## Checking for the duplicate tracks and dopping them
 
 print(songs['Track'].duplicated().sum())
 songs.drop_duplicates(subset=['Track'], inplace=True)
 
 print(songs['Track'].duplicated().sum())
+
+## Checking the Distribution
+    ## Release Date Distribution
+sns.histplot(songs['Release Date'], kde=True, bins = 10)
+plt.title("Distribution of Songs by Release Date")
+plt.xlabel("Release Date")
+plt.ylabel("Number of Songs")
+plt.show()
+
+    ## Release Month Distribution
+month_distr = songs.groupby('Release Month')['Track'].count().reset_index()
+print(month_distr)
+sns.barplot(x = month_distr['Release Month'], y = month_distr['Track'], palette= 'coolwarm')
+plt.title("Distribution of Songs by Release Month")
+plt.xlabel("Release Month")
+plt.ylabel("Number of Songs")
+plt.show()
+
+    ## Artist Distribution - Top 20 Artists
+top_artist = songs['Artist'].value_counts().head(20)
+sns.barplot(x=top_artist.values, y = top_artist.index, palette='vlag')
+plt.title("Top 20 Artists by Number of Songs")
+plt.xlabel("Number of Songs")
+plt.ylabel("Artist")
+plt.show()
+
+
 
 ## Top 10 tracks on Different Platforms 
     ## Top 10 Tracks by Popularity on Spotify
@@ -71,3 +100,4 @@ for col in cols_to_convert:
     plt.show()
 
 
+## Analyzing How the Popularity of Songs Changes Over Time
